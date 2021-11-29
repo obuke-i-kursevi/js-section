64238118c7770899fa1c8b2fddca3d67e6987c73
m.bojkic <m.bojkic@emakina.rs>
2021-11-29 21:43:29 +0100

    24.cas (29.11.) - String concatination, Scopes, Functions

--- grupa1/js-calculator-with-basics/assets/practice.js
+++ grupa1/js-calculator-with-basics/assets/practice.js
@@ -1,3 +1,7 @@
+// 1. cas 
+
+console.log('-------------------- 1. cas --------------------')
+
 // ! VARIJABLE (promenljive)
 
 // alert('Hello World!');
@@ -111,3 +115,143 @@ operationStr = 'a + 25 - 40 - a / 2 + 5 * 3';
 
 console.log(myOperation);
 console.log(operationStr);
+
+
+// 2. cas (29.11.2021.)
+
+console.log('----------------------- 2. cas ------------------------');
+
+let myMathOperation = (a / 10) + (a * 5) - 10;
+
+let myMathOperationString = '(' + a + ' / 10) + (' + a + ' * 5) - 10';
+
+console.log(myMathOperation);
+
+console.log(myMathOperationString);
+
+// ! 1. zadatak za vezbu
+// Iskoristiti res varijablu (njenu vrednost) kako bi se prikazao string format sa tom vrednoscu
+
+let res = 10;
+
+let firstTaskOperation = 'res + (res*3) - (10+res)';
+
+let firstTaskOperationString = res + ' + (' + res + '*3) - (10+' + res + ')';
+
+console.log(firstTaskOperationString);
+
+let copyOfFirstTaskOperation = firstTaskOperation;
+
+console.log(firstTaskOperation);
+
+console.log(copyOfFirstTaskOperation);
+
+firstTaskOperation = 555;
+
+console.log(firstTaskOperation);
+
+console.log(copyOfFirstTaskOperation);
+
+firstTaskOperation = 'res + (res*3) - (10+res)';
+
+firstTaskOperationString = "'('" + res + ' + (' + res + '*3) - (10+' + res + ')';
+
+let templateLiteralsVariable = `${res} + (${res}*3) - (10+${res})`;
+
+console.log(templateLiteralsVariable);
+
+// ! 2. zadatak
+// Zapisati u Template Literals formatu varijablu:
+
+let secondTaskOperation = 'res * (15*res) - (res/2) + res';
+
+let secondTaskOperationString = `${res} * (15 * ${res}) - (${res}/ 2) + ${res}`;
+
+console.log(secondTaskOperationString);
+
+let goToNewLineString = `My string can go \nto the next line.`;
+
+console.log(goToNewLineString);
+
+// ! FUNKCIJE
+
+function helloWorld(planetName) {
+    alert(`Hello planet ${planetName}`); // 1. nacin
+    // alert('Hello planet ' + planetName); // 2. nacin
+}
+
+// helloWorld('Jupiter');
+
+// helloWorld('Earth');
+
+// helloWorld('Mars');
+
+const result = 'Hello JS!';
+
+function add(number1, number2) {
+    const result = number1 + number2;
+    console.log(`Testing result variable: ${result}`);
+    return `Result of add function is ${result}`;
+    // return `Result of add function ${number1+number2}`;
+}
+
+console.log(result);
+
+console.log(add(50,100));
+
+console.log(add(3, 10));
+
+let anotherX;
+anotherX = 55;
+
+function printVariable() {
+    console.log(`testing anotherX variable: ${anotherX}`);
+}
+
+printVariable();
+
+// TODO   ->    DOMACI zadaci sa drugog casa
+
+// 1. zadatak
+/**
+ *  Kreirati dve varijable. Jedna neka bude tipa number i neka se zove userInput (na koju god vrednost je postavite) i druga neka se naziva      currentResult i nemojte je postaviti ni na kakvu vrednost.
+    Postavite currentResult varijablu na vrednost userInput plus neki broj (npr 20).
+    U naredne 3 linije koda, varijablu currentResult promenite u svakoj narednoj liniji koda operacijama oduzimanja, mnozenja i deljenja nad tom currentResult varijablom i svaki put ispisati vrednost varijable na konzoli u formatu koji zelite s tim da pre toga stoji prefiks `Current result is: `
+    Na kraju varijablu currentResult ispisati u okviru alert prozora, a userInput varijablu ispisati samo na konzoli.
+ * 
+*/
+
+let userInput = 120;
+let currentResult;
+
+currentResult = userInput + 20;
+
+currentResult = currentResult - 10;
+
+console.log(`Current result is: ${currentResult}`);
+
+currentResult = currentResult * 5;
+
+console.log(`Current result is: ${currentResult}`);
+
+currentResult = currentResult / 2;
+
+console.log(`Current result is: ${currentResult}`);
+
+alert(`Current result is: ${currentResult}`);
+
+console.log(`User input value is: ${userInput}`);
+
+// 2. zadatak
+/** 
+ * Kreirati funkciju square() koja ce broj prosledjen kao parametar podici na kvadrat.
+   Rezultat vratiti u formatu ‘Result is: broj’.
+   Funkciji kao argument proslediti odredjeni broj (npr 5).
+*/
+
+function squareFunc(number) {
+    const result = number**2;
+    return `Result is: ${result}`;
+}
+
+console.log(squareFunc(5));
--- grupa2/js-calculator-with-basics/assets/practice.js
+++ grupa2/js-calculator-with-basics/assets/practice.js
@@ -1,3 +1,7 @@
+// ! 1. cas (24.11.2021.)
+
+console.log('--------------------- 1. cas ---------------------');
+
 // alert('Hello World!');
 
 // ! VARIJABLE (promenljive)
@@ -81,8 +85,8 @@ let number2 = (number1 + 50) * 3 + 13;
 
 console.log(number2);
 
-// ! Zadaci za vezbu
-// 1.
+// TODO  -->  Zadaci za vezbu
+// ! 1. zadatak
 /* 
     * kreirati varijable proizvoljnog naziva u dozvoljenom camelCase formatu (3 do 4)
     * kreirati ih uz kombinaciju LET i CONST rezervisanih reci
@@ -100,7 +104,7 @@ console.log(firstVar);
 console.log(secondVar);
 console.log(personName);
 
-// 2.
+// ! 2. zadatak
 /*
     * Napraviti 2 varijable uz LET ili CONST (sami smislite koju je bolje iskoristiti)
     * Prva varijabla neka cuva rezultat neke matematicke operacije po izboru (slozenije)
@@ -111,7 +115,150 @@ let a = 20;
 let operation;
 
 const myOperation = a + 25 - 40 - a / 2 + 5 * 3;
-operationStr = 'a + 25 - 40 - a / 2 + 5 * 3';
+let operationStr = 'a + 25 - 40 - a / 2 + 5 * 3';
 
 console.log(myOperation);
 console.log(operationStr);
+
+
+// ! 2. cas (29.11.2021.)
+
+console.log('--------------------- 2. cas ---------------------');
+
+operationStr = a + ' + 25 - 40 - ' + a + ' / 2 + 5 * 3';
+
+// Zadatak za vezbu
+
+let firstTaskOperation = 'a + (a * 3) - (10 + a)';
+
+firstTaskOperation = a + ' + (' + a + ' * 3) - (10 + ' + a + ')';
+
+console.log(firstTaskOperation);
+
+let test1Var = 'Testing JavaScript';
+
+let copyOfTest1Var = test1Var;
+
+console.log(test1Var);
+console.log(copyOfTest1Var);
+
+test1Var = 1050;
+
+console.log(test1Var);
+console.log(copyOfTest1Var);
+
+let myValue = 50;
+
+let temp = myValue;
+
+myValue = 'bilo sta bukvalno';
+
+let test2Var = 'a + (a * 3) - (10 + a)';
+
+test2Var = "a + (a * 3) - (10 + a)";
+
+let templateLiteralVariable = `${a} + (${a} * 3) - (10 + ${a})`;
+
+console.log(firstTaskOperation);
+console.log(templateLiteralVariable);
+
+// Zadatak za vezbu
+
+let test3Var = 'a - (15*a) / a';
+test3Var = `${a} - (15*${a}) / ${a}`;
+
+console.log(test3Var);
+
+let goToNewLineString = 'Go to new line \nfrom this point of string';
+
+console.log(goToNewLineString);
+
+
+// ! Funkcije
+
+function helloWorld(planetName) {
+    alert(`Hello planet ${planetName}`);
+}
+
+// helloWorld('Jupiter');
+
+// helloWorld('Earth');
+
+// helloWorld('Mars');
+
+const result = 'Some random variable';
+
+function add(number1, number2) {
+    const result = number1 + number2;
+    console.log(`My result is: ${result}`);
+    return `Result of add function is ${result}`;
+    // return `Result of add function is ${number1+number2}`;
+}
+
+// console.log(result);
+
+console.log(add(30,50));
+
+console.log(add(7000, 25000));
+
+let test4Var;
+
+test4Var = 'Some random text';
+
+const myConstVariable = 55;
+
+console.log(test4Var);
+
+function printMyName() {
+    console.log(myNameVar);
+}
+
+let myNameVar = 'Marko Bojkic';
+
+printMyName();
+
+// TODO   ->    DOMACI zadaci sa drugog casa
+
+// 1. zadatak
+/**
+ *  Kreirati dve varijable. Jedna neka bude tipa number i neka se zove userInput (na koju god vrednost je postavite) i druga neka se naziva      currentResult i nemojte je postaviti ni na kakvu vrednost.
+    Postavite currentResult varijablu na vrednost userInput plus neki broj (npr 20).
+    U naredne 3 linije koda, varijablu currentResult promenite u svakoj narednoj liniji koda operacijama oduzimanja, mnozenja i deljenja nad tom currentResult varijablom i svaki put ispisati vrednost varijable na konzoli u formatu koji zelite s tim da pre toga stoji prefiks `Current result is: `
+    Na kraju varijablu currentResult ispisati u okviru alert prozora, a userInput varijablu ispisati samo na konzoli.
+ * 
+*/
+
+let userInput = 120;
+let currentResult;
+
+currentResult = userInput + 20;
+
+currentResult = currentResult - 10;
+
+console.log(`Current result is: ${currentResult}`);
+
+currentResult = currentResult * 5;
+
+console.log(`Current result is: ${currentResult}`);
+
+currentResult = currentResult / 2;
+
+console.log(`Current result is: ${currentResult}`);
+
+alert(`Current result is: ${currentResult}`);
+
+console.log(`User input value is: ${userInput}`);
+
+// 2. zadatak
+/** 
+ * Kreirati funkciju square() koja ce broj prosledjen kao parametar podici na kvadrat.
+   Rezultat vratiti u formatu ‘Result is: broj’.
+   Funkciji kao argument proslediti odredjeni broj (npr 5).
+*/
+
+function squareFunc(number) {
+    const result = number**2;
+    return `Result is: ${result}`;
+}
+
+console.log(squareFunc(5));
